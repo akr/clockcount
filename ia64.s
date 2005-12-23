a13e6351704b14f2fcbdee607729e95fc4859c11
Tanaka Akira <akr@fsij.org>
2005-12-23 17:12:07 +0000

    .mib removed.

--- ia64.s
+++ ia64.s
@@ -1,16 +1,15 @@
-	.file	"ia64.s"
-	.pred.safe_across_calls p1-p5,p16-p63
-	.text
-	.align 16
-	.global clockcount#
-	.proc clockcount#
+        .file   "ia64.s"
+        .pred.safe_across_calls p1-p5,p16-p63
+        .text   
+        .align 16
+        .global clockcount#
+        .proc clockcount#
 clockcount:
-	.prologue
-	.body
+        .prologue
+        .body
         mov r8 = ar44
-	;;
-	.mib
-	nop.m 0
-	nop.i 0
-	br.ret.sptk.many b0
-	.endp clockcount#
+        ;;
+        nop 0
+        nop 0
+        br.ret.sptk.many b0
+        .endp clockcount#
